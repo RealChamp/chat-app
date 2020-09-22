71928a23d321d0b4d851ff3de5d00dbb7e4e5400
Matvey <matvey.w2015@mail.ru>
2020-09-22 23:54:24 +0400

    upgrade styles for message component and component themself

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,38 +1,48 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import distanceInWordsToNow from 'date-fns/distance_in_words_to_now'
-import ruLocale from 'date-fns/locale/ru'
-import './Message.scss'
-import classNames from 'classnames'
+import React from 'react';
+import PropTypes from 'prop-types';
+import distanceInWordsToNow from 'date-fns/distance_in_words_to_now';
+import ruLocale from 'date-fns/locale/ru';
+import './Message.scss';
+// import classNames from 'classnames'
+import checkedMessage from '../../assets/img/double-checked.svg';
+import uncheckedMessage from '../../assets/img/checked.svg'
 
-function Message({avatar, text, date, isMe}) { 
-    return(
-        <div className={isMe ? 'message message--isme' : 'message'}>
-            <div className="message__avatar">
-                <img src={avatar} alt={`Avatar`}/>
-                {/* TODO user.fullname in alt */}
-            </div>
-            <div className="message__content">
-                <div className="message__bubble">
-                    <p className='message__text'>{text}</p>
-                </div>
-                <span className="message__date">
-                    {distanceInWordsToNow(date, {addSuffix: true, locale: ruLocale})}
-                </span>
-            </div>
+
+function Message({ avatar, text, date, isMe, isReaded }) {
+  return (
+    <div className={!isMe ? 'message message--isme' : 'message'}>
+      <div className="message__content">
+      <div className="message__avatar">
+        <img src={avatar} alt={`Avatar`} />
+        {/* TODO user.fullname in alt */}
+      </div>
+        <div className="message__bubble">
+          <p className="message__text">{text}</p>
         </div>
-    )
-} 
-Message.defaultProps = {
-    user: {
-    }
+        {isReaded ? (
+            <img className='checked-icon' src={checkedMessage} alt=""/>
+        ) : (
+            <img className='unchecked-icon' src={uncheckedMessage} alt=""/>
+        )
+        
+         }
+       
+      </div>
+        <span className="message__date">
+          {distanceInWordsToNow(date, { addSuffix: true, locale: ruLocale })}
+        </span>
+    </div>
+  );
 }
+Message.defaultProps = {
+  user: {},
+};
 
 Message.propTypes = {
-    avatar: PropTypes.string,
-    text: PropTypes.string,
-    date: PropTypes.string,
-    user: PropTypes.object,
-}
+  avatar: PropTypes.string,
+  text: PropTypes.string,
+  date: PropTypes.string,
+  user: PropTypes.object,
+};
 
-export default Message
+export default Message;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,8 +5,8 @@ import './Home.scss'
 function Home() {
     return (
       <section className="home">
-       <Message avatar='https://randomuser.me/api/portraits/women/44.jpg' text='Hello' date='Tue Sep 22 2020 19:58:20' isMe={true}/>
-       <Message avatar='https://randomuser.me/api/portraits/women/43.jpg' text='Hello World Test' date='Tue Sep 22 2020 19:58:20' isMe={false}/>
+       <Message avatar='https://randomuser.me/api/portraits/women/44.jpg' text='Hello' date='Tue Sep 22 2020 19:58:20' isMe={true} isReaded={false}/>
+       <Message avatar='https://randomuser.me/api/portraits/women/43.jpg' text='Hello World Test' date='Tue Sep 22 2020 19:58:20' isMe={false} isReaded={true}/>
       </section>
     );
 }
