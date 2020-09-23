69a5e2eb55fe7b0ea28bd24469ec309df016b469
Matvey <matvey.w2015@mail.ru>
2020-09-23 19:44:48 +0400

    add attachments in message component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
-export {default as Button} from './Button/Button'
-export {default as Block} from './Block/Block'
-export {default as Message} from './Message/Message'
+export {default as Button} from './Button'
+export {default as Block} from './Block'
+export {default as Message} from './Message'
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,54 @@
-import React from 'react'
-import Message from '../../components/Message/Message'
-import './Home.scss'
+import React from 'react';
+import Message from '../../components/Message';
+import './Home.scss';
 
 function Home() {
-    return (
-      <section className="home">
-       <Message avatar='https://randomuser.me/api/portraits/women/44.jpg' text='Hello' date='Tue Sep 22 2020 19:58:20' isMe={true} isReaded={false}/>
-       <Message avatar='https://randomuser.me/api/portraits/women/43.jpg' text='Hello World Test' date='Tue Sep 22 2020 19:58:20' isMe={false} isReaded={true}/>
-      </section>
-    );
+  return (
+    <section className="home">
+      <Message
+        avatar="https://randomuser.me/api/portraits/women/44.jpg"
+        text="Hello"
+        date="Tue Sep 22 2020 19:58:20"
+        isMe={true}
+        isReaded={false}
+        attachments={[
+           {
+             filename: 'image.jpg',
+             url: 'https://source.unsplash.com/100x100/?random=1&nature,water',
+           },
+           {
+             filename: 'image.jpg',
+             url: 'https://source.unsplash.com/100x100/?random=2&nature,water',
+           },
+           {
+             filename: 'image.jpg',
+             url: 'https://source.unsplash.com/100x100/?random=3&nature,water',
+           }
+        ]}
+      />
+      <Message
+        avatar="https://randomuser.me/api/portraits/women/43.jpg"
+        text="Hello World Test"
+        date="Tue Sep 22 2020 19:58:20"
+        isMe={false}
+        isReaded={true}
+        attachments={[
+           {
+             filename: 'image.jpg',
+             url: 'https://source.unsplash.com/100x100/?random=1&nature,water',
+           },
+           {
+             filename: 'image.jpg',
+             url: 'https://source.unsplash.com/100x100/?random=2&nature,water',
+           },
+           {
+             filename: 'image.jpg',
+             url: 'https://source.unsplash.com/100x100/?random=3&nature,water',
+           }
+        ]}
+      />
+    </section>
+  );
 }
 
-export default Home    
+export default Home;
